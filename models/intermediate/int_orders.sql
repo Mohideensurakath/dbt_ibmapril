9590f349d6fc3b880096266d16994508faf2ee7a
Mohideen Surakath <sms05july04@gmail.com>
2026-05-21 10:44:54 +0000

    gg

--- models/intermediate/int_orders.sql
+++ models/intermediate/int_orders.sql
@@ -1,23 +1,30 @@
 -- SELECT *
 -- FROM {{ ref('stg_orders') }}
 
-{{ config(materialized='table') }}
+{{ config(materialized='incremental',unique_key='order_id') }}
+
 SELECT
     order_id,
+    order_date,
     customer_id,
-    comment,
     clerk_name,
-
     -- USD
-    total_price AS inventory_value_usd,
-
+    total_price AS total_price_usd,
     -- EUR
     {{ usd_eur('total_price') }}
-        AS inventory_value_eur,
-
+        AS total_price_eur,
     status_code,
     priority_code,
     ship_priority,
-    order_date
-
+    comment,
+    upd_date
 FROM {{ ref('stg_orders') }}
+
+
+{% if dbt.is_incremental is false %}
+WHERE upd_date >
+(
+    SELECT MAX(upd_date)
+    FROM {{ this }}
+)
+{% endif %}
